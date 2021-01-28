3715a3befaa7e7fdca9a29ff983dbff13997778d
Helena Wenzin <helma254@hotmail.com>
2021-01-28 09:55:47 +0100

    MeasureControllerTests

--- src/main/java/se/wenzin/foodiecalc/model/Measure.java
+++ src/main/java/se/wenzin/foodiecalc/model/Measure.java
@@ -1,6 +1,11 @@
 package se.wenzin.foodiecalc.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -9,33 +14,34 @@ import java.util.UUID;
 public class Measure implements Serializable {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID")
     private UUID uuid;
 
-    @Column(name = "UNITNAME")
-    private String unitName;
+    @Column(name = "NAME")
+    private String name;
 
-    public Measure() {}
+    public Measure() {
+    }
 
-    public Measure(UUID uuid, String unitName) {
+    public Measure(UUID uuid, String name) {
         this.uuid = uuid;
-        this.unitName = unitName;
+        this.name = name;
     }
 
     public UUID getId() {
         return uuid;
     }
 
-    public void setId(UUID id) {
+    public void setId(UUID uuid) {
         this.uuid = uuid;
     }
 
-    public String getUnitName() {
-        return unitName;
+    public String getName() {
+        return name;
     }
 
-    public void setUnitName(String unitName) {
-        this.unitName = unitName;
+    public void setName(String name) {
+        this.name = name;
     }
 }
